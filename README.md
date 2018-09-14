08731845c606028e613c548cedbe7525aee8a743
lu22do <lpierre@LudoMac.local>
2018-09-13 22:01:29 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Overview
-Webpack 4+Phaser+React based boilerplate.
+Tile game.
 
 # Commands
-  * 'npm run dev' to start in dev mode.
-  * 'npm run build' to build for deployment (output in dist/ folder).
+
+* 'npm run dev' to start in dev mode.
+* 'npm run build' to build for deployment (output in dist/ folder).
+
+# Sources
+
+* Based on https://github.com/lu22do/webpack4-react-phaser-boilerplate.git.
+* Implements on https://medium.com/@michaelwesthadley/modular-game-worlds-in-phaser-3-tilemaps-1-958fc7e6bbd6.
+* [Tuxemon](https://github.com/Tuxemon/Tuxemon) tileset, sprites and level by the Tuxemon team.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import greet from './greet';
 import './style.css';
 import 'phaser';
-import logo from './assets/logo.png';
+import tiles from './assets/tilesets/tuxmon-sample-32px-extruded.png';
+import map from './assets/tilemaps/tuxemon-town.json';
+import atlas1 from './assets/atlas/atlas.png';
+import atlas2 from './assets/atlas/atlas.json';
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -14,30 +17,155 @@ ReactDOM.render(<Index />, document.getElementById("index"));
 
 var config = {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  parent: 'phaser',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
+let cursors;
+let player;
+let showDebug = false;
 
 function preload () {
-  this.load.image('logo', logo);
+  this.load.image('tiles', tiles);
+  this.load.tilemapTiledJSON('map', map);
+  this.load.atlas("atlas", atlas1, atlas2);
 }
 
 function create () {
-  var logo = this.add.image(400, 150, 'logo');
+  const map = this.make.tilemap({ key: "map" });
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: 'Power2',
-    yoyo: true,
-    loop: -1
+  const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+  // Parameters: layer name (or index) from Tiled, tileset, x, y
+  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+  worldLayer.setCollisionByProperty({ collides: true });
+
+  aboveLayer.setDepth(10);
+
+  const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+  player = this.physics.add
+    .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+    .setSize(30, 40)
+    .setOffset(0, 24);
+
+  this.physics.add.collider(player, worldLayer);
+
+  const anims = this.anims;
+  anims.create({
+    key: "misa-left-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
   });
+  anims.create({
+    key: "misa-right-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-front-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-back-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  this.add
+    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+      font: "18px monospace",
+      fill: "#000000",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#ffffff"
+    })
+    .setScrollFactor(0)
+    .setDepth(30);
+
+  this.input.keyboard.once("keydown_D", event => {
+    // Turn on physics debugging to show player's hitbox
+    this.physics.world.createDebugGraphic();
+
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    worldLayer.renderDebug(graphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
+  });
+}
+
+function update(time, delta) {
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  // Stop any previous movement from the last frame
+  player.body.setVelocity(0);
+
+  // Horizontal movement
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(speed);
+  }
+
+  // Vertical movement
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(speed);
+  }
+
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  player.body.velocity.normalize().scale(speed);
+
+  // Update the animation last and give left/right animations precedence over up/down animations
+  if (cursors.left.isDown) {
+    player.anims.play("misa-left-walk", true);
+  } else if (cursors.right.isDown) {
+    player.anims.play("misa-right-walk", true);
+  } else if (cursors.up.isDown) {
+    player.anims.play("misa-back-walk", true);
+  } else if (cursors.down.isDown) {
+    player.anims.play("misa-front-walk", true);
+  } else {
+    player.anims.stop();
+
+    // If we were moving, pick and idle frame to use
+    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+    else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+    else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+    else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+  }
 }
